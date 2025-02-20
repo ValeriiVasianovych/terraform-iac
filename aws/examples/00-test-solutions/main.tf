4527d6e31d7064daaf075926c6b40196f6509088
Valerii Vasianovych <valerii.vasianovych.2003@gmail.com>
2025-02-20 01:31:55 +0100

    add new files

--- aws/examples/00-test-solutions/main.tf
+++ aws/examples/00-test-solutions/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  # cloud {
+  #   organization = "terraform-cloud-local"
+
+  #   workspaces {
+  #     name = "cli-driven-workspace" 
+  #   }
+  # }
+  backend "s3" {
+    bucket = "terrafrom-tfstate-file-s3-bucket"
+    # dynamodb_table = "terraform-tfstate-dynamodb"
+    encrypt      = true
+    key          = "aws/tfstates/test-solutions/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
 # Datasources
 data "aws_ami" "latest_ubuntu" {
   owners      = ["099720109477"]
@@ -21,15 +39,15 @@ variable "environment" {
   default     = "dev"
 }
 
-# variable "instance_types" {
-#   type        = map(string)
-#   description = "The types of instances to launch"
-#   default = {
-#     dev     = "t2.micro"
-#     staging = "t2.medium"
-#     prod    = "t2.large"
-#   }
-# }
+variable "instance_types" {
+  type        = map(string)
+  description = "The types of instances to launch"
+  default = {
+    dev     = "t2.micro"
+    staging = "t2.medium"
+    prod    = "t2.large"
+  }
+}
 
 locals {
   file_content = file("${path.module}/hello-tf.txt") # Path module is the directory where the current module is located
@@ -51,29 +69,80 @@ provider "aws" {
 
 # Resources
 # resource "aws_instance" "example_ec2" {
-#   ami               = data.aws_ami.latest_ubuntu.id
-# #   instance_type     = lookup(var.instance_types, var.environment, "t3.micro")
-#   instance_type = (terraform.workspace == "dev" ? "t3.micro" : "t2.medium")
-#   availability_zone = "${var.region}-a"
-#   key_name          = "ServersKey"
+#   ami           = data.aws_ami.latest_ubuntu.id
+#   instance_type = lookup(var.instance_types, var.environment, "t3.micro")
+#   # instance_type = (terraform.workspace == "dev" ? "t3.micro" : "t2.medium")
+#   availability_zone      = module.vpc.azs[0]
+#   key_name               = "ServersKey"
+#   vpc_security_group_ids = [aws_security_group.tf-sg.id]
+
 #   tags = merge(var.common_tags, {
 #     Name = "ec2-instance"
 #   })
+
+#   provisioner "file" {
+#     source      = "${path.module}/data.json"
+#     destination = "/tmp/data.json"
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "cd /tmp",
+#       "ls -al",
+#       "cat data.json",
+#       "ps -ef"
+#     ]
+#   }
+
+#   provisioner "local-exec" {
+#     command = "echo 'Instance created!'"
+#   }
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = file("~/.ssh/ServersKey.pem")
+#     host        = self.public_ip
+#   }
 # }
 
-resource "aws_security_group" "example_sg" {
-  name        = "example_sg"
+resource "aws_s3_bucket" "tf-s3-bucket" {
+  bucket = "terraform-s3-bucket-vasianovych-2"
+  # depends_on = [aws_security_group.tf-sg]
+}
+
+resource "aws_security_group" "tf-sg" {
+  name        = "tf-sg"
   description = "An example security group for Terraform"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+  dynamic "ingress" {
+    for_each = [80, 443, 22, 3306]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name  = "example_sg"
+    Name  = "tf-sg"
     Owner = "Valerii Vasianovych"
+  }
+
+  lifecycle {
+    replace_triggered_by = [aws_s3_bucket.tf-s3-bucket] # This security group will be replaced when the s3 bucket is replaced
+    #create_before_destroy = true          # Creates a new security group before destroying the old one if the resource is being replaced
+    #ignore_changes = ["ingress", "name"]  # ingnores changes in the ingress and name attributes
+    #prevent_destroy = true                # Prevents the security group from being destroyed. It will return an error if you try to destroy it
+
   }
 }
 
@@ -89,3 +158,27 @@ resource "aws_security_group" "example_sg" {
 output "file_content" {
   value = local.file_content
 }
+
+output "multiply" {
+  value = 2 * 7
+}
+
+output "greater_than" {
+  value = 5 > 4
+}
+
+resource "aws_security_group" "web" {
+  name = "web-sg"
+
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow traffic on port ${ingress.value} for ${aws_security_group.web.name}"
+    }
+  }
+}
+
--- aws/examples/00-test-solutions/vpc.tf
+++ aws/examples/00-test-solutions/vpc.tf
@@ -4,15 +4,14 @@
 #   name = "test-vpc"
 #   cidr = "10.0.0.0/16"
 
-#   azs             = ["eu-west-1a", "eu-west-1b"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+#   azs             = ["${var.region}a", "${var.region}b"]
+#   private_subnets = []
+#   public_subnets  = ["10.0.10.0/24", "10.0.20.0/24"]
 
-#   enable_nat_gateway = true
+#   enable_nat_gateway = false
 #   enable_vpn_gateway = false
 
 #   tags = {
 #     Terraform = "true"
-#     Environment = "dev"
 #   }
 # }
